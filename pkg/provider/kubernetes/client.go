4cfb83eb2af19aca03e2e3c12cee482cc81de2aa
Moritz Johner <beller.moritz@googlemail.com>
2024-06-18 22:28:08 +0200

    chore: move k8s/metadata into its own file
    Signed-off-by: Moritz Johner <beller.moritz@googlemail.com>

--- pkg/provider/kubernetes/client.go
+++ pkg/provider/kubernetes/client.go
@@ -23,12 +23,10 @@ import (
 
 	"github.com/tidwall/gjson"
 	v1 "k8s.io/api/core/v1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"sigs.k8s.io/yaml"
 
 	esv1beta1 "github.com/external-secrets/external-secrets/apis/externalsecrets/v1beta1"
 	"github.com/external-secrets/external-secrets/pkg/constants"
@@ -496,133 +494,4 @@ func getFromSecretMetadata(secret *v1.Secret, ref esv1beta1.ExternalSecretDataRe
 	}
 
 	return []byte(val.String()), true, nil
-}
-
-const (
-	metadataAPIVersion = "kubernetes.external-secrets.io/v1alpha1"
-	metadataKind       = "PushSecretMetadata"
-)
-
-type PushSecretMetadata struct {
-	metav1.TypeMeta
-	Spec PushSecretMetadataSpec `json:"spec,omitempty"`
-}
-type PushSecretMetadataSpec struct {
-	TargetMergePolicy targetMergePolicy `json:"targetMergePolicy,omitempty"`
-	SourceMergePolicy sourceMergePolicy `json:"sourceMergePolicy,omitempty"`
-
-	Labels      map[string]string `json:"labels,omitempty"`
-	Annotations map[string]string `json:"annotations,omitempty"`
-}
-
-type targetMergePolicy string
-
-const (
-	targetMergePolicyMerge   targetMergePolicy = "Merge"
-	targetMergePolicyReplace targetMergePolicy = "Replace"
-	targetMergePolicyIgnore  targetMergePolicy = "Ignore"
-)
-
-type sourceMergePolicy string
-
-const (
-	sourceMergePolicyMerge   sourceMergePolicy = "Merge"
-	sourceMergePolicyReplace sourceMergePolicy = "Replace"
-)
-
-func parseMetadataParameters(data *apiextensionsv1.JSON) (*PushSecretMetadata, error) {
-	if data == nil {
-		return nil, nil
-	}
-	var metadata PushSecretMetadata
-	err := yaml.Unmarshal(data.Raw, &metadata, yaml.DisallowUnknownFields)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse %s %s: %w", metadataAPIVersion, metadataKind, err)
-	}
-
-	if metadata.APIVersion != metadataAPIVersion {
-		return nil, fmt.Errorf("unexpected apiVersion %q, expected %q", metadata.APIVersion, metadataAPIVersion)
-	}
-
-	if metadata.Kind != metadataKind {
-		return nil, fmt.Errorf("unexpected kind %q, expected %q", metadata.Kind, metadataKind)
-	}
-
-	return &metadata, nil
-}
-
-// Takes the local secret metadata and merges it with the push metadata.
-// The push metadata takes precedence.
-// Depending on the policy, we either merge or overwrite the metadata from the local secret.
-func mergeSourceMetadata(localSecret *v1.Secret, pushMeta *PushSecretMetadata) (map[string]string, map[string]string, error) {
-	labels := localSecret.ObjectMeta.Labels
-	annotations := localSecret.ObjectMeta.Annotations
-	if pushMeta == nil {
-		return labels, annotations, nil
-	}
-	if labels == nil {
-		labels = make(map[string]string)
-	}
-	if annotations == nil {
-		annotations = make(map[string]string)
-	}
-
-	switch pushMeta.Spec.SourceMergePolicy {
-	case "":
-		// default to merge
-		fallthrough
-	case sourceMergePolicyMerge:
-		for k, v := range pushMeta.Spec.Labels {
-			labels[k] = v
-		}
-		for k, v := range pushMeta.Spec.Annotations {
-			annotations[k] = v
-		}
-	case sourceMergePolicyReplace:
-		labels = pushMeta.Spec.Labels
-		annotations = pushMeta.Spec.Annotations
-	default:
-		return nil, nil, fmt.Errorf("unexpected source merge policy %q", pushMeta.Spec.SourceMergePolicy)
-	}
-	return labels, annotations, nil
-}
-
-// Takes the remote secret metadata and merges it with the source metadata.
-// The source metadata may replace the existing labels/annotations
-// or merge into it depending on policy.
-func mergeTargetMetadata(remoteSecret *v1.Secret, pushMeta *PushSecretMetadata, sourceLabels, sourceAnnotations map[string]string) (map[string]string, map[string]string, error) {
-	labels := remoteSecret.ObjectMeta.Labels
-	annotations := remoteSecret.ObjectMeta.Annotations
-	if pushMeta == nil {
-		return labels, annotations, nil
-	}
-
-	if labels == nil {
-		labels = make(map[string]string)
-	}
-	if annotations == nil {
-		annotations = make(map[string]string)
-	}
-
-	switch pushMeta.Spec.TargetMergePolicy {
-	case "":
-		fallthrough
-	case targetMergePolicyMerge:
-		for k, v := range sourceLabels {
-			labels[k] = v
-		}
-		for k, v := range sourceAnnotations {
-			annotations[k] = v
-		}
-	case targetMergePolicyReplace:
-		labels = sourceLabels
-		annotations = sourceAnnotations
-	case targetMergePolicyIgnore:
-		// leave the target metadata as is
-		// this is useful when we only want to push data
-		// and the user does not want to touch the metadata
-	default:
-		return nil, nil, fmt.Errorf("unexpected target merge policy %q", pushMeta.Spec.TargetMergePolicy)
-	}
-	return labels, annotations, nil
 }
